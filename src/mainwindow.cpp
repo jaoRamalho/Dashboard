d5ace7830b2b7e5b7dc33b084911a9a9dacb17ac
jaoPedro <joaoramalho@alunos.utfpr.edu.br>
2025-06-29 21:51:01 -0300

    Arrumando click na linha de process

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -130,7 +130,7 @@ MainWindow::~MainWindow(){
 }
 
 void MainWindow::onProcessTableRowClicked(int row) {
-    std::lock_guard<std::mutex> lock(globalMutex); // Protege o acesso à lista de processos
+    //std::lock_guard<std::mutex> lock(globalMutex); // Protege o acesso à lista de processos
     if (row < 0 || row >= ui->processTable->rowCount() || !update) {
         return; // Verifica se a linha é válida
     }
